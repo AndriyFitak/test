adabfb124b31362396825f9a684ccceab9dc2264
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-07-25 18:45:41 +0200

    js

--- script.js
+++ script.js
@@ -55,10 +55,11 @@ const renderProducts = () => {
   products.forEach(productItem => { 
       productsEl.innerHTML += `
       <div product-id="${productItem.id}" class="products_item">
-          <p>${productItem.name}</p>
-          <p>${productItem.quantity}</p>
-          <p>${productItem.price}</p>
-          <p>${productItem.description}</p>
+          <p>Імя: ${productItem.name}</p>
+          <p>Айді: ${productItem.id}</p>
+          <p>Кількість: ${productItem.quantity}</p>
+          <p>Ціна: ${productItem.price}</p>
+          <p>Опис: ${productItem.description}</p>
           <button product-id="${productItem.id}" class="btnDelete">Delete</button>
       </div>
       `
@@ -79,17 +80,30 @@ const renderProducts = () => {
 }
 
 
-const idProduct = "RD1pT9tZ"
-axios.get(`http://localhost:4000/products/get-item?id=${idProduct}`)
-.then((res) => {
-    console.log(res)
-})
+// const idProduct = "RD1pT9tZ"
+// axios.get(`http://localhost:4000/products/get-item?id=${idProduct}`)
+// .then((res) => {
+//     console.log(res)
+// })
 
 
-axios.get(`http://localhost:4000/products/get?page=2&limit=10`)
-.then((res) => {
-    console.log(res)
-})
+// axios.get(`http://localhost:4000/products/get?page=1&limit=10`)
+// .then((res) => {
+//     console.log(res)
+// })
+
+
+const btnCount = document.querySelector('.btnCount');
+btnCount.addEventListener('click', () => {
+    axios.get('http://localhost:4000/products/count')
+        .then((res) => {
+            const resultParagraph = document.getElementById('result');
+            resultParagraph.innerText = `Кількість продуктів: ${res.data.count}`;
+        })
+        .catch((error) => {
+            console.error('Помилка отримання даних:', error);
+        });
+});
 
 
 
@@ -116,3 +130,37 @@ formCreate.btn.addEventListener('click', () => {
 })
 
   
+
+
+
+const formChange = {
+    name: document.querySelector('#form_name_change'),
+    id: document.querySelector('#form_id')
+}
+
+const changeBtn = document.querySelector('#form_btnChange');
+changeBtn.addEventListener('click', () => {
+    const changeData = {
+        name: formChange.name.value,
+        id: formChange.id.value
+    }
+    axios.put(`http://localhost:4000/products/change-name?id=${changeData.id}&name=${changeData.name}`)
+    .then((res) => {
+        console.log(res.data)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
